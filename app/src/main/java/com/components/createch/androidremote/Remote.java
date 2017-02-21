f40848f79391c67500e7bde4525f2cc6d684334f
HorisoftProject <mathieukostiuk@hotmail.fr>
2017-02-21 17:11:00 +0100

    First commit

---
+++ app/src/main/java/com/components/createch/androidremote/Remote.java
@@ -0,0 +1,13 @@
+package com.components.createch.androidremote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Remote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote);
+    }
+}
