0fabb64e7cba85849ec23be91c556e7ffc44d12e
HorisoftProject <mathieukostiuk@hotmail.fr>
2017-02-22 12:28:19 +0100

    Android_Remote-3 Mise en place de la couche UpNp
    Une partie de l'implémentation UPnP est en place.
    Le device apparaît sur le réseau et nous avons accès aux fonctions UPnP de base.
    Il ne manque plus qu'à implémanter les fonctions UPnP liées à l'interface graphique

--- app/src/main/java/com/components/createch/androidremote/Remote.java
+++ app/src/main/java/com/components/createch/androidremote/Remote.java
@@ -1,13 +1,135 @@
 package com.components.createch.androidremote;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class Remote extends AppCompatActivity {
+import org.fourthline.cling.android.AndroidUpnpService;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+import org.fourthline.cling.binding.LocalServiceBindingException;
+import org.fourthline.cling.binding.annotations.AnnotationLocalServiceBinder;
+import org.fourthline.cling.model.DefaultServiceManager;
+import org.fourthline.cling.model.ValidationException;
+import org.fourthline.cling.model.meta.DeviceDetails;
+import org.fourthline.cling.model.meta.DeviceIdentity;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.LocalService;
+import org.fourthline.cling.model.meta.ManufacturerDetails;
+import org.fourthline.cling.model.meta.ModelDetails;
+import org.fourthline.cling.model.types.DeviceType;
+import org.fourthline.cling.model.types.UDADeviceType;
+import org.fourthline.cling.model.types.UDAServiceType;
+import org.fourthline.cling.model.types.UDN;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class Remote extends AppCompatActivity implements PropertyChangeListener {
+
+    private AndroidUpnpService upnpService;
+
+    private UDN udn = new UDN(UUID.randomUUID()); // TODO: Not stable! Sauvegarder l'UUID dans un fichier après la première génération
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            upnpService = (AndroidUpnpService) service;
+
+            LocalService<RemoteController> remoteControllerService = getRemoteControllerService();
+
+            // Register the device when this activity binds to the service for the first time
+            if (remoteControllerService == null) {
+                try {
+                    LocalDevice remoteDevice = createDevice();
+
+                    upnpService.getRegistry().addDevice(remoteDevice);
+
+                    remoteControllerService = getRemoteControllerService();
+
+                } catch (Exception ex) {
+                    System.err.println("Creating Android remote controller device failed !!!");
+                    return;
+                }
+            }
+
+           /* // Obtain the state of the power switch and update the UI
+            setLightbulb(switchPowerService.getManager().getImplementation().getStatus());
+
+            // Start monitoring the power switch
+            switchPowerService.getManager().getImplementation().getPropertyChangeSupport()
+                    .addPropertyChangeListener(LightActivity.this);*/
+
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            upnpService = null;
+        }
+    };
+
+
+    protected LocalDevice createDevice()
+            throws ValidationException, LocalServiceBindingException {
+
+        DeviceType type =
+                new UDADeviceType("RemoteController", 1);
+
+        DeviceDetails details =
+                new DeviceDetails(
+                        "Android Remote Controller",
+                        new ManufacturerDetails("Creative Technology"),
+                        new ModelDetails("AndroidController", "a UPnP Android based remote controller", "v1")
+                );
+
+        LocalService service =
+                new AnnotationLocalServiceBinder().read(RemoteController.class);
+
+        service.setManager(
+                new DefaultServiceManager<>(service, RemoteController.class)
+        );
+
+        return new LocalDevice(
+                new DeviceIdentity(udn),
+                type,
+                details,
+
+                service
+        );
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
+
+        getApplicationContext().bindService(
+                new Intent(this, AndroidUpnpServiceImpl.class),
+                serviceConnection,
+                Context.BIND_AUTO_CREATE
+        );
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        //TODO!!!
+    }
+
+    protected LocalService<RemoteController> getRemoteControllerService() {
+        if (upnpService == null)
+            return null;
+
+        LocalDevice binaryLightDevice;
+        if ((binaryLightDevice = upnpService.getRegistry().getLocalDevice(udn, true)) == null)
+            return null;
+
+        return (LocalService<RemoteController>)
+                binaryLightDevice.findService(new UDAServiceType("RemoteController", 1));
     }
 }
