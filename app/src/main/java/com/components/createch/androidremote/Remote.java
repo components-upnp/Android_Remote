f67f17b82e58e587dd60049e15f067d76223dc8a
MKostiuk <Mat01061995>
2017-05-19 11:43:18 +0200

    Suppression TitleBar

--- app/src/main/java/com/components/createch/androidremote/Remote.java
+++ app/src/main/java/com/components/createch/androidremote/Remote.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.SeekBar;
 
 import org.fourthline.cling.android.AndroidUpnpServiceImpl;
